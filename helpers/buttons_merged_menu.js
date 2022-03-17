e6cf23854d7ceef5997a0d164f5e0318fee33a4f
regorxxx <regorxxx@protonmail.com>
2022-03-17 15:20:27 +0100

    - Buttons: Playlist Tools buttons now are shown in their own sub-folder at the button bar 'Add buttons' menu.

--- helpers/buttons_merged_menu.js
+++ helpers/buttons_merged_menu.js
@@ -1,5 +1,5 @@
 ﻿'use strict'
-//22/02/22
+//17/03/22
 
 include('menu_xxx.js');
 include('helpers_xxx.js');
@@ -14,9 +14,9 @@ function createButtonsMenu(name) {
 	menu.newEntry({entryText: 'Toolbar configuration:', func: null, flags: MF_GRAYED});
 	menu.newEntry({entryText: 'sep'});
 	if (!_isFolder(folders.data)) {_createFolder(folders.data);}
-	const notAllowedDup = new Set(['buttons_playlist_tools.js', 'buttons_playlist_history.js', 'buttons_playlist_tools_macros.js', 'buttons_tags_automation.js', 'buttons_playlist_tools_pool.js', 'buttons_others_device_priority.js', 'buttons_tags_save_tags.js', 'buttons_tags_automation.js', 'buttons_tags_fingerprint_chromaprint.js', 'buttons_tags_fingerprint_fooid.js', 'buttons_search_fingerprint_chromaprint.js','buttons_search_fingerprint_chromaprint_fast.js', 'buttons_search_fingerprint_fooid.js','buttons_fingerprint_tools.js']);
+	const notAllowedDup = new Set(['buttons_playlist_tools.js', 'buttons_playlist_history.js', 'buttons_playlist_tools_macros.js', 'buttons_playlist_tools_pool.js', 'buttons_others_device_priority.js', 'buttons_tags_save_tags.js', 'buttons_tags_fingerprint_chromaprint.js', 'buttons_tags_fingerprint_fooid.js', 'buttons_search_fingerprint_chromaprint.js','buttons_search_fingerprint_chromaprint_fast.js', 'buttons_search_fingerprint_fooid.js','buttons_fingerprint_tools.js']);
 	const requirePlaylistTools = new Set(['buttons_playlist_tools_macros.js', 'buttons_playlist_tools_macro_custom.js', 'buttons_playlist_tools_pool.js', 'buttons_playlist_tools_submenu_custom.js']);
-	const subCategories = ['_fingerprint_', '_search_', '_tags_', '_playlist_', '_others_']; // By order of priority if it matches multiple strings
+	const subCategories = ['_fingerprint_', '_search_', '_tags_', '_playlist_tools', '_playlist_', '_others_']; // By order of priority if it matches multiple strings
 	const buttonsPathNames = new Set(buttonsPath.map((path) => {return path.split('\\').pop();}));
 	function isAllowed(fileName) {return !notAllowedDup.has(fileName) || !buttonsPathNames.has(fileName);}
 	function isAllowedV2(fileName) {return !requirePlaylistTools.has(fileName) || buttonsPathNames.has('buttons_playlist_tools.js');}
@@ -28,7 +28,7 @@ function createButtonsMenu(name) {
 			let entryText = path.split('\\').pop() + (isAllowed(fileName) ? (isAllowedV2(fileName) ? '' : '\t(Playlist Tools)') : '\t(1 allowed)');
 			let subMenuFolder = subCategories.find((folder) => {return entryText.indexOf(folder) !== -1;});
 			if (subMenuFolder && subMenuFolder.length) {
-				 subMenuFolder = capitalizeAll(subMenuFolder.replace(/[_]/g,'')) + invId;
+				 subMenuFolder = (subMenuFolder === '_playlist_tools' ? 'Playlist Tools' : capitalizeAll(subMenuFolder.replace(/[_]/g,''))) + invId;
 				if (!menu.hasMenu(subMenuFolder, subMenu)) {menu.newMenu(subMenuFolder, subMenu);}
 			}
 			entryText = entryText.replace('buttons_', '');
