4e549e4b874c4b34a94569c957c4784943846513
regorxxx <regorxxx@protonmail.com>
2022-03-28 20:37:55 +0200

    Don't log to CMD console.

--- helpers-external/ffmpeg/ffmpeg.bat
+++ helpers-external/ffmpeg/ffmpeg.bat
@@ -1,4 +1,4 @@
 REM Helper to execute ffmpeg.exe (%3) on input (%1) and save output to file (%2)
 REM Retrieves LRA data as json
-
+@ECHO OFF
 %3 -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null -  2>&1 | > %2 FINDSTR /BIR "{ .*\" }"
--- helpers-external/fpcalc/fpcalc.bat
+++ helpers-external/fpcalc/fpcalc.bat
@@ -1,4 +1,4 @@
 REM Helper to execute fpcalc.exe (%3) on input (%1) and save output to file (%2)
 REM Retrieves raw fingerprint as json
-
+@ECHO OFF
 %3 -raw -json -length 240 %1>%2
