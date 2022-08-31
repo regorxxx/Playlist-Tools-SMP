b9176a022eb86b20d2844b77678badd9fd3fd694
regorxxx <regorxxx@protonmail.com>
2022-08-31 18:00:39 +0200

    - Script Integration\SMP Dynamic menu: now replaces all functionality previously found at Script Integration\SMP Main menu. Custom menus and functions may still be added, and device/DSP selector is no longer required (since it requires CMD and therefore can also be changed directly with it). - Online controllers integration: reworked and updated all code for full integration with [foo_httpcontrol](https://hydrogenaud.io/index.php/topic,62218.0.html) has been added when using the preset [ajquery-xxx](https://github.com/regorxxx/ajquery-xxx) now using dynamic menus.

--- main/main_menu_custom.js
+++ main/main_menu_custom.js
@@ -1,12 +1,13 @@
 ﻿'use strict';
-//13/10/21
+//28/08/22
 
 include('..\\helpers\\helpers_xxx.js');
+include('..\\helpers\\callbacks_xxx.js');
 
-const onMainMenuEntries = {
-	'1': {name: 'Add skip Tag at current playback', funcName: 'skipTagFromPlayback', path: folders.xxx + 'main\\skip_tag_from_playback.js', icon: 'ui-icon ui-icon-tag'},
-	'2': {name: 'Pools Top Tracks Mix', funcName: 'Pools\\Top tracks mix', menuName: 'menu', icon: 'ui-icon ui-icon-circle-zoomout'},
-};
+const onMainMenuEntries = [
+	{name: 'Add skip Tag at current playback', funcName: 'skipTagFromPlayback', path: folders.xxx + 'main\\skip_tag_from_playback.js', icon: 'ui-icon ui-icon-tag'},
+	{name: 'Execute menu entry by name', funcName: 'executeByName' , path: '', icon: 'ui-icon ui-icon-star'}
+];
 
 const onMainMenuDynamicEntries = [];
 
@@ -16,29 +17,25 @@ function deleteMainMenuDynamic() {
 }
 
 // Callback
-// Meant to replace any other
-function on_main_menu(idx) {
-    if (onMainMenuEntries.hasOwnProperty(idx)) {
-		const entry = onMainMenuEntries[idx];
-		console.log('SMP main menu ' + idx + ': ' + entry.name);
-		if (entry.hasOwnProperty('path')) {
-			if (entry.path.length) {
-				include(entry.path);
-				entry.path = '';
-			}
-			try {eval(entry.funcName)();} 
-			catch (e) {console.log('Error evaluating: ' + entry.funcName + ' from script (' + (entry.path.length ? entry.path : 'parent') + ').');}
-		} else if (entry.hasOwnProperty('menuName')) {
-			try {eval(entry.menuName).btn_up(void(0), void(0), void(0), entry.funcName);} 
-			catch (e) {console.log('Error evaluating: ' + entry.funcName + ' from menu (' + entry.menuName + ').');}
-		}
-	}
-}
-
-function on_main_menu_dynamic(idx) {
+addEventListener('on_main_menu_dynamic', (idx) => {
 	if (idx < onMainMenuDynamicEntries.length) {
 		const entry = onMainMenuDynamicEntries[idx];
-		try {menu.btn_up(void(0), void(0), void(0), entry.name);}
-		catch (e) {console.log('Error evaluating: ' + entry.name + '.');}
+		if (entry.onMainMenuEntries) {
+			console.log('SMP main menu ' + idx + ': ' + entry.name);
+			if (entry.hasOwnProperty('path')) {
+				if (entry.path.length) {
+					include(entry.path);
+					entry.path = '';
+				}
+				try {eval(entry.funcName)();} 
+				catch (e) {console.log('Error evaluating: ' + entry.funcName + ' from script (' + (entry.path.length ? entry.path : 'parent') + ').');}
+			} else if (entry.hasOwnProperty('menuName')) {
+				try {eval(entry.menuName).btn_up(void(0), void(0), void(0), entry.funcName);} 
+				catch (e) {console.log('Error evaluating: ' + entry.funcName + ' from menu (' + entry.menuName + ').');}
+			}
+		} else {
+			try {menu.btn_up(void(0), void(0), void(0), entry.name);}
+			catch (e) {console.log('Error evaluating: ' + entry.name + '.');}
+		}
 	}
-}
+});
