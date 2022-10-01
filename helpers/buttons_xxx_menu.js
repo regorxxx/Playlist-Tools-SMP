a307c2d4d3cc40e96d61156128662f77f6a449e5
regorxxx <regorxxx@protonmail.com>
2022-10-01 14:18:56 +0200

    - Buttons: readme is now available in config menus for individual buttons too (and not only the toolbar).

--- helpers/buttons_xxx_menu.js
+++ helpers/buttons_xxx_menu.js
@@ -3,11 +3,13 @@
 
 include('menu_xxx.js');
 include('helpers_xxx_properties.js')
+include('helpers_xxx_file.js');
 
-function settingsMenu(parent, bShowValues = false) {
+function settingsMenu(parent, bShowValues = false, readmeFiles = []) {
 	const menu = new _menu();
 	const properties = parent.buttonsProperties;
 	const parentName = isFunction(parent.text) ? parent.text(parent) : parent.text;
+	const readmeList = readmeFiles.length && _isFile(folders.xxx + 'helpers\\readme\\buttons_list.json') ? _jsonParseFileCheck(folders.xxx + 'helpers\\readme\\buttons_list.json', 'Readme list', window.Name, utf8) : null;
 	// Menu
 	menu.newEntry({entryText: 'Configurate button:', func: null, flags: MF_GRAYED});
 	menu.newEntry({entryText: 'sep'});
@@ -38,6 +40,34 @@ function settingsMenu(parent, bShowValues = false) {
 				overwriteProperties(properties); // Updates panel
 			}});
 		});
+	}
+	menu.newEntry({entryText: 'sep'});
+	menu.newEntry({entryText: 'Restore defaults', func: () => {
+		const options = Object.keys(properties);
+		options.forEach((key) => {properties[key][1] = properties[key][3];});
+		overwriteProperties(properties); // Updates panel
+	}});
+	if (readmeList) {
+		menu.newEntry({entryText: 'sep'});
+		if (readmeFiles.length > 1) {
+			readmeFiles.forEach((name) => {
+				const readmeFile = readmeList.hasOwnProperty(name) ? readmeList[name] : '';
+				if (readmeFile.length) {
+					menu.newEntry({entryText: readmeFile, func: () => {
+						const readme = _open(folders.xxx + 'helpers\\readme\\' + readmeFile, utf8);
+						if (readme.length) {fb.ShowPopupMessage(readme, readmeFile);}
+						else {console.log(readmeFile + ' not found.');}
+					}});
+				}
+			});
+		} else {
+			menu.newEntry({entryText: 'Readme...', func: () => {
+				const readmeFile = readmeList.hasOwnProperty(readmeFiles[0]) ? readmeList[readmeFiles[0]] : '';
+				const readme = readmeFile.length ? _open(folders.xxx + 'helpers\\readme\\' + readmeFile, utf8) : '';
+				if (readme.length) {fb.ShowPopupMessage(readme, readmeFile);}
+				else {console.log(readmeFile + ' not found.');}
+			}});
+		}
 	}
 	return menu;
 }
