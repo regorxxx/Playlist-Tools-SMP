1d02cde3699e033e829952db661161ac27cace95
regorxxx <regorxxx@protonmail.com>
2022-10-19 01:52:39 +0200

    Code cleanup.

--- main/harmonic_mixing.js
+++ main/harmonic_mixing.js
@@ -19,7 +19,7 @@ function harmonicMixing({
 								selItems = plman.GetPlaylistSelectedItems(plman.ActivePlaylist),
 								playlistLength = selItems.Count, 
 								playlistName = 'Harmonic mix from ' + plman.GetPlaylistName(plman.ActivePlaylist),
-								keyTag = 'KEY',
+								keyTag = typeof globTags !== 'undefined' ? globTags.key : 'KEY',
 								bSendToPls = true,
 								bDoublePass = false,
 								bDebug = false,
