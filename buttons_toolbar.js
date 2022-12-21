7ad27295db083ded0e1629c330ae78a28fe22481
regorxxx <regorxxx@protonmail.com>
2022-12-21 19:53:07 +0100

    - Buttons: default method of installation requires now to load the toolbar (no more single buttons support), from there, any button can be loaded as desired. - Buttons: the buttons bar now shows a message when no buttons have been added, left clicking shows a popup with available buttons presets. Right clicking opens the menu to configure the toolbar or add buttons manually.

--- buttons_toolbar.js
+++ buttons_toolbar.js
@@ -1,26 +1,16 @@
 ﻿'use strict';
-//10/10/22
+//21/12/22
 
-/*
-	This is an example of how merging buttons works. Just include them...
+/* Playlist Tools: Buttons Toolbar
+	Loads any button found on the buttons folder. Just load this file and add your desired buttons via R. Click.
+	Compatible with:
+		- Search by Distance
+		- Playlist Tools
+		- Fingerprint Tools
+		- ListenBrainz Tools
+		...
 	
-	-	Note every button file has a line that adds the buttons from the file to the glonal list, so it always merges the new buttons with the previous ones.
-			buttons = {...buttons, ...newButtons};
-		
-	-	First included will be first (left or top), last ones will be last (right or bottom).
-		
-	-	You can add copies of the same button just by including them multiple times. As is, without touching anything on the button files.
-		All IDs and property names are automatically changed accordingly (with a count). That means, copies of buttons will be differents instances of
-		the same button for all purposes (with their own properties).
-		
-	-	You can change orientation for all buttons just by changing 'x' to 'y'. Width (w) and height (h) can also be set.
-		
-	-	You can change size for specific buttons too but beware: if changing width on 'y' orientation, then you should just change the global width.
-		Otherwise, you will have some buttons with the default width and others will be wider... and that looks really weird on vertical orientation.
-		Same applies to height for horizontal orientation, better to apply the same height to all buttons, not specific ones, for 'x' orientation.
-		
-	-	Instead of adding buttons at the end with include functions, you can add your own paths to the array. If some file doesn't exist, then it just gets
-		skipped, instead of throwing and error! This is better than try/catch, since it doesn't omit coding errors while including them...
+	Loading single buttons scripts in a panel is allowed but not supported.
 */
 
 var bLoadTags = true; // Note this must be added before loading helpers! See buttons_search_by_tags_combinations.js and search_same_by.js
@@ -33,16 +23,26 @@ var bLoadTags = true; // Note this must be added before loading helpers! See but
 		'helpers\\helpers_xxx_properties.js',
 		'helpers\\helpers_xxx_UI.js',
 		'helpers\\helpers_xxx_file.js',
-		'helpers\\buttons_merged_menu.js'];
+		'helpers\\helpers_xxx_input.js'
+	];
 	let bIncludeRel = true;
 	try {include('..\\..\\helpers\\helpers_xxx_dummy.js');} catch(e) {bIncludeRel = false;}
 	if (bIncludeRel) {dependencies.forEach((file) => {include('..\\..\\' + file);});}
 	else {dependencies.forEach((file) => {include(file);});}
 }
+{
+	const dependencies = [
+		'helpers\\buttons_merged_menu.js'
+	];
+	let bIncludeRel = true;
+	try {include('..\\helpers\\buttons_dummy.js');} catch(e) {bIncludeRel = false;}
+	if (bIncludeRel) {dependencies.forEach((file) => {include('..\\' + file);});}
+	else {dependencies.forEach((file) => {include('buttons\\' + file);});}
+}
 
 try {window.DefinePanel('Playlist Tools: Buttons Bar', {author:'XXX', version: '3.0.0-beta.16', features: {drag_n_drop: false}});} catch (e) {} //May be loaded along other buttons
 
-let barProperties = { //You can simply add new properties here
+let barProperties = {
 	name:				['Name of config json file', 'buttons_' + randomString(5)],
 	toolbarColor: 		['Toolbar color', -1],
 	textColor:	 		['Buttons\' text color', buttonsBar.config.textColor],
@@ -56,33 +56,21 @@ let barProperties = { //You can simply add new properties here
 	activeColor:		['Buttons\' active icon color', buttonsBar.config.activeColor],
 	animationColors:	['Buttons\' animation colors', JSON.stringify(buttonsBar.config.animationColors)],
 };
-setProperties(barProperties); //This sets all the panel properties at once
+setProperties(barProperties);
 barProperties = getPropertiesPairs(barProperties);
 
-// Toolbar menu
-buttonsBar.menu = () => {
-	return createButtonsMenu(barProperties.name[1]);
-};
-
-// First popup
-if (!barProperties.firstPopup[1]) {
-	const readmePath = folders.xxx + 'helpers\\readme\\toolbar.txt';
-	barProperties.firstPopup[1] = true;
-	overwriteProperties(barProperties); // Updates panel
-	const readme = _open(readmePath, utf8);
-	if (readme.length) {fb.ShowPopupMessage(readme, 'Toolbar');}
-}
-
 // Config at buttons_xxx.js
+// Toolbar menu
+buttonsBar.menu = () => {return createButtonsMenu(barProperties.name[1]);};
 // Global toolbar color
 buttonsBar.config.toolbarColor = barProperties.toolbarColor[1];
-buttonsBar.config.bToolbar = buttonsBar.config.toolbarColor !== -1 ? true : false; // Change this on buttons bars files to set the background color
+buttonsBar.config.bToolbar = buttonsBar.config.toolbarColor !== -1 ? true : false; // To set the background color
 buttonsBar.config.partAndStateID = barProperties.bBgButtons[1] ? 1 : 6;
 buttonsBar.config.textColor = barProperties.textColor[1];
 buttonsBar.config.activeColor = barProperties.activeColor[1];
 buttonsBar.config.animationColors = JSON.parse(barProperties.animationColors[1]);
 // Show Id on tooltips
-buttonsBar.config.bShowID = barProperties.bShowId[1]; // Change this on buttons bars files to set the background color
+buttonsBar.config.bShowID = barProperties.bShowId[1];
 // Orientation
 buttonsBar.config.orientation = barProperties.orientation[1];
 // Reflow
@@ -94,43 +82,38 @@ buttonsBar.config.toolbarTooltip = 'R. Click to configure...\nHold R. Click to m
 // Assign size
 buttonsBar.config.scale =  barProperties.scale[1];
 
-// Buttons
-const buttonsPathDef = [	 // Add here your buttons names
-					'buttons_search_by_tags_combinations.js',
-					'buttons_playlist_remove_duplicates.js',
-					'buttons_search_bydistance.js',
-					'buttons_search_bydistance_customizable.js',
-					'buttons_playlist_tools.js',
-					'buttons_playlist_history.js',
-				].map((name) => {return folders.xxx + 'buttons\\' + name;});
-let buttonsPath = [...buttonsPathDef];
-
-loadButtonsFile();
-{
-	const bProcessed = new Set();
-	for (let i = 0; i < buttonsPath.length; i++) {
-		if (_isFile(buttonsPath[i])) {
-			include(buttonsPath[i], {always_evaluate: true});
-			const newKeys = [];
-			Object.keys(buttonsBar.buttons).forEach((key) => {
-				if (!bProcessed.has(key)) {
-					bProcessed.add(key);
-					newKeys.push(key);
-				}
-			});
-			buttonsBar.listKeys.push(newKeys);
-		} else {
-			console.log(buttonsPath[i] +' not loaded');
-		}
-	}
-	console.log('Buttons loaded: ' + buttonsBar.listKeys.flat(Infinity).join(', '));
+// First popup
+if (!barProperties.firstPopup[1]) {
+	buttonsBar.firstPopup = true; // For later use
+	const readmePath = folders.xxx + 'helpers\\readme\\toolbar.txt';
+	barProperties.firstPopup[1] = true;
+	overwriteProperties(barProperties); // Updates panel
+	const readme = _open(readmePath, utf8);
+	if (readme.length) {fb.ShowPopupMessage(readme, 'Toolbar');}
 }
 
-function loadButtonsFile() {
+// Load Buttons
+function loadButtonsFile(bStartup = false) {
 	let names = [];
 	const file = folders.data + barProperties.name[1] + '.json';
 	if (!_isFile(file)) {
-		names = buttonsPath.map((path) => {return path.split('\\').pop();});
+		if (bStartup && !buttonsBar.firstPopup) {return false;}
+		// Show popup with presets
+		const presets = [
+			{name: 'Playlist Tools', files: ['buttons_playlist_tools.js', 'buttons_playlist_tools_submenu_custom.js', 'buttons_playlist_tools_macros.js','buttons_playlist_tools_pool.js']},
+			{name: 'Search by Distance', files: ['buttons_search_by_distance.js']},
+			{name: 'Search by Distance (customizable)', files: ['buttons_search_by_distance_customizable.js', 'buttons_search_by_distance_customizable.js', 'buttons_search_by_distance_customizable.js','buttons_search_by_distance_customizable.js']},
+			{name: 'Top Tracks', files: ['buttons_search_top_tracks.js', 'buttons_search_top_tracks_from_date.js', 'buttons_search_top_tracks_from_date.js']},
+			{name: 'Library search', files: ['buttons_search_by_tags_combinations.js', 'buttons_search_by_tags_queries.js', 'buttons_search_quicksearch.js']},
+			{name: 'Playlist manipulation', files: ['buttons_playlist_remove_duplicates.js', 'buttons_playlist_filter.js', 'buttons_playlist_filter.js', 'buttons_playlist_history.js']},
+			{name: 'Device priority', files: ['buttons_others_device_priority.js', 'buttons_others_device_selector.js']},
+			{name: 'Full (no Search by Distance)', files: ['buttons_playlist_tools.js', 'buttons_playlist_tools_submenu_custom.js','buttons_playlist_tools_macros.js', 'buttons_search_by_tags_combinations.js','buttons_playlist_remove_duplicates.js', 'buttons_playlist_filter.js','buttons_search_quicksearch.js']},
+			{name: 'Full', files: ['buttons_playlist_tools.js', 'buttons_playlist_tools_submenu_custom.js','buttons_search_by_distance_customizable.js', 'buttons_search_by_distance_customizable.js','buttons_playlist_remove_duplicates.js', 'buttons_playlist_filter.js', 'buttons_search_quicksearch.js']},
+			{name: 'Blank', files: []}
+		].map((preset) => {return (preset.files.every((file) => _isFile(folders.xxx + 'buttons\\' + file)) ? preset : void(0));}).filter(Boolean);
+		const input = Input.number('int positive', presets.length, 'Choose a preset (by number) from the following list, to load the toolbar with pre-defined buttons (they may be added/removed at any time later):\n' + presets.map((p, i) => '\t' + _b(i + 1) + ' ' + p.name).join('\n') + '\n\nCanceling will load a blank toolbar by default.', 'Toolbar: preset', 1, [(n) => n > 0 && n <= presets.length]);
+		if (input == null) {return false;}
+		names = presets[input - 1].files.map((path) => {return path.split('\\').pop();});
 		_save(file, JSON.stringify(names, null, '\t'));
 	} else {
 		const data = _jsonParseFileCheck(file, 'Buttons bar', window.Name, utf8);
@@ -138,4 +121,40 @@ function loadButtonsFile() {
 		if (!isArrayEqual(data, names)) {_save(file, JSON.stringify(names, null, '\t'));} // Rewrite file for older versions with full paths TODO
 	}
 	buttonsPath = names.map((name) => {return folders.xxx + 'buttons\\' + name;});
+	return buttonsPath.length;
 }
+
+function includeButtons() {
+	if (buttonsPath.length) {
+		const bProcessed = new Set();
+		for (let i = 0; i < buttonsPath.length; i++) {
+			if (_isFile(buttonsPath[i])) {
+				include(buttonsPath[i], {always_evaluate: true});
+				const newKeys = [];
+				Object.keys(buttonsBar.buttons).forEach((key) => {
+					if (!bProcessed.has(key)) {
+						bProcessed.add(key);
+						newKeys.push(key);
+					}
+				});
+				buttonsBar.listKeys.push(newKeys);
+			} else {
+				console.log(buttonsPath[i] + ' not loaded');
+			}
+		}
+		console.log('Buttons loaded: ' + buttonsBar.listKeys.flat(Infinity).join(', '));
+	}
+}
+
+let buttonsPath = [];
+loadButtonsFile(true) && includeButtons();
+
+addEventListener('on_paint', (gr) => {
+	if (!buttonsPath.length) {
+		gr.GdiDrawText('L. Click to load a preset / R. Click to add buttons manually', _gdiFont('Segoe UI', _scale(10)), 0xFF000000, 0, 0, window.Width, window.Height, DT_VCENTER | DT_CENTER | DT_END_ELLIPSIS | DT_CALCRECT | DT_NOPREFIX); // Font is being cached, no problems here...
+	}
+});
+
+addEventListener('on_mouse_lbtn_up', (x, y, mask) => {
+	!buttonsPath.length && loadButtonsFile() && includeButtons() && window.Repaint();
+});
