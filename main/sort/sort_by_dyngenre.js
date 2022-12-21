3517bbef2be8e5059fa4a06e6547be6d8f500dad
regorxxx <regorxxx@protonmail.com>
2022-12-21 19:48:49 +0100

    Structure cleanup.

---
+++ main/sort/sort_by_dyngenre.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+//19/12/22
+
+/*	
+	Sort by Dyngenre
+	-----------------------------------
+	Uses notation described at 'dyngenre_map' on dyngenre_map_xxx.js to sort selection by genre/style.
+*/ 
+
+include('..\\..\\helpers\\dyngenre_map_xxx.js');
+
+const [,, genreStyleMap] = dyngenreMap();
+
+function sortByDyngenre({
+								playlistIdx = plman.ActivePlaylist,
+								styleGenreTag = typeof globTags !== 'undefined' ? [globTags.genre, globTags.style] : ['GENRE', 'STYLE'],
+								bSelection = true,
+								sortOrder = 1,
+								bAscii = true,
+								bDebug = false
+							} = {}) {
+	// Safety checks
+	if (!styleGenreTag.length) {return false;}
+	if (playlistIdx >= plman.PlaylistCount) {return false;}
+	if (!plman.PlaylistItemCount(playlistIdx)) {return false;}
+	if (bSelection && !plman.GetPlaylistSelectedItems(playlistIdx).Count) {return false;}
+	// List
+	const handleList = bSelection ? plman.GetPlaylistSelectedItems(playlistIdx) : plman.GetPlaylistItems(playlistIdx);
+	const count = handleList.Count;
+	// Tags
+	const idTfo = '[%PATH%]-[%SUBSONG%]';
+	const ids = fb.TitleFormat(idTfo).EvalWithMetadbs(handleList);
+	const styleGenre = getTagsValuesV3(handleList, styleGenreTag, true);
+	let dyngenre = [[]];
+	for (let i = 0; i < count; i++) {
+		const styleGenre_i = Array.from(new Set((styleGenre[i] ? styleGenre[i].filter(Boolean) : [])));
+		const styleGenreLength = styleGenre_i.length;
+		let dyngenreNum = 0;
+		if (styleGenreLength) {
+			for (let j = 0; j < styleGenreLength; j++) {
+				let dyngenre_j = genreStyleMap.get(bAscii ? _asciify(styleGenre_i[j]) : styleGenre_i[j]);
+				if (dyngenre_j) {
+					let k;
+					let dyngenre_j_length = dyngenre_j.length;
+					for (k = 0; k < dyngenre_j_length; k++) {
+						if (!dyngenre[i]) {dyngenre[i] = [];}
+						dyngenre[i].push(dyngenre_j[k]);
+					}
+				}
+			}
+			dyngenreNum = dyngenre[i] ? dyngenre[i].length : 0;
+		}
+		if (dyngenreNum) {
+			dyngenre[i] = dyngenre[i].reduce((prev, next) => {return prev + next;}) / dyngenreNum;
+		} else {
+			dyngenre[i] = Infinity; // Not matched tracks are put at the end
+		}
+	}
+	let tfo = '';
+	// Translate into something usable on TF
+	// Can not do the same than sortByKey() because track may have multiple genre/styles
+	// Therefore it would require a giant tfo comparing each value and then dividing by total number...
+	// Better to identify each track and assign a value to it.
+	// Also, instead of adding multiple individual if statements, better to nest them (so only those required are evaluated)
+	dyngenre.forEach ( (val, index) => {
+		const sortVal = -(sortOrder === -1 ? 999999999 - dyngenre[index] : dyngenre[index]);
+		tfo += '$if($stricmp(' + idTfo + ',' + sanitizeTagTfo(ids[index]).replace(/,/g,'\',\'') + '),' + sortVal + ',';
+	});
+	dyngenre.forEach ( () => {tfo += ')';}); // Add closures!
+	if (bDebug) {console.log(tfo);}
+	plman.UndoBackup(plman.ActivePlaylist);
+	return plman.SortByFormat(playlistIdx, tfo, bSelection);
+}
---
+++ main/tags/skip_tag_from_playback.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+//16/11/22
+
+/* 
+	Add Skip Tag From Playback
+	Adds a 'SKIP' tag using current playback. Meant to be used along Skip Track (foo_skip) component.
+	Has an intelligent switch which sets behavior according to playback time:
+		- If time > half track length -> Track will play as usually up to the 'SKIP' time, where it jumps to next track.
+		- If time < half track length -> Track will play from 'SKIP' time to the end.
+	This is a workaround for using %playback_time% for tagging, since %playback_time% does not work within masstagger scripts.
+ */
+
+function skipTagFromPlayback(selItem = new FbMetadbHandleList(fb.GetNowPlaying())) {
+	if (typeof selItem !== 'undefined' && selItem !== null) {
+		const countItems = selItem.Count;
+		if (countItems === 0) {
+			console.log('No tracks selected.');
+			return;
+		}
+		if (countItems > 1) {
+			console.log('More than 1 track selected, playback time can only be used for one track at once.');
+			return;
+		}
+	} else {return;}
+	const bAppend = utils.IsKeyPressed(0x10); // Append tag instead of replace when pressing shift
+	const currentPlayback = fb.PlaybackTime * 1000;
+	const time = new Date(currentPlayback).toUTCString().substr(20,5) + '.00'; // doesn't care about ms
+	const bEnd = currentPlayback > selItem[0].Length * 1000 / 2 ? true : false; // skips from start or end
+	const SKIP = [];
+	if (bAppend) {
+		const fileInfo = selItem[0].GetFileInfo();
+		if (fileInfo) {
+			const idx = fileInfo.MetaFind('SKIP');
+			const count = fileInfo.MetaValueCount(idx);
+			for (let i = 0; i < count; i++) {
+				SKIP.push(fileInfo.MetaValue(idx, i));
+			}
+		}
+	}
+	SKIP.push((bEnd ? '' : '-') + time);
+	selItem.UpdateFileInfoFromJSON(JSON.stringify([{SKIP}]));
+	console.log((bAppend ? 'Adding' : 'Setting') + ' SKIP tag on current track: ' + time + (bEnd ? ' (skips end)' : ' (skips start)'));
+	if (bEnd) {fb.Next();}
+	return time;
+}
