4ca489d640f2130e0aae41f2248a5b93156acd55
regorxxx <regorxxx@protonmail.com>
2023-01-08 14:03:22 +0100

    - Other Tools\Write tags: LRA tagging now uses [Sed](https://gnuwin32.sourceforge.net/packages/sed.htm) tool to extract the tag, which seems to give less problems with some tracks. This is due to a buggy behavior of ffmpeg which outputs a full tag report whenever the tool is used... binaries are already included on download. In case one desires to use the old FINDSTR command instead, just change useSed=TRUE to FALSE on the bat files at '.\helpers-external\ffmpeg'.

--- helpers-external/ffmpeg/ffmpeg.bat
+++ helpers-external/ffmpeg/ffmpeg.bat
@@ -3,11 +3,21 @@ REM Helper to execute ffmpeg.exe (%3) on input (%1) and save output to file (%2)
 REM Retrieves LRA data as json
 SET ffPath=%3
 SET arch=x64
+SET sed=%ffPath:ffmpeg.exe=sed.exe%
+SET useSed=TRUE
 IF "%PROCESSOR_ARCHITECTURE%" == "x86" ( 
-    IF NOT DEFINED PROCESSOR_ARCHITEW6432 (set arch=x86)
+	IF NOT DEFINED PROCESSOR_ARCHITEW6432 (SET arch=x86)
 ) 
 IF "%arch%" == "x64" (
-	%ffPath% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null -  2>&1 | > %2 FINDSTR /BIR "{ .*\" }"
+	IF "%useSed%" == "TRUE" (
+		%ffPath% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null - 2>&1 | > %2 %sed% 1,/^\[Parsed_loudnorm/d
+	) ELSE (
+		%ffPath% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null - 2>&1 | > %2 FINDSTR /BIR "{ .*\" }"
+	)
 ) ELSE (
-	%ffPath:.exe=_32.exe% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null -  2>&1 | > %2 FINDSTR /BIR "{ .*\" }"
+	IF "%useSed%" == "TRUE" (
+		%ffPath:.exe=_32.exe% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null - 2>&1 | > %2 %sed% 1,/^\[Parsed_loudnorm/d
+	) ELSE (
+		%ffPath:.exe=_32.exe% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null - 2>&1 | > %2 FINDSTR /BIR "{ .*\" }"
+	)
 )
--- helpers-external/ffmpeg/ffmpeg_rs.bat
+++ helpers-external/ffmpeg/ffmpeg_rs.bat
@@ -1,16 +1,24 @@
 @ECHO OFF
 REM Helper to execute ffmpeg.exe on input (%1) and show console
 REM Retrieves LRA data as json
-set path=%~dp0
+SET path=%~dp0
 SET arch=x64
-set ffmpeg="%path%ffmpeg.exe"
+SET ffmpeg="%path%ffmpeg.exe"
+SET sed="%path%sed.exe"
+SET useSed=TRUE
 IF "%PROCESSOR_ARCHITECTURE%" == "x86" ( 
-    IF NOT DEFINED PROCESSOR_ARCHITEW6432 (set arch=x86)
+	IF NOT DEFINED PROCESSOR_ARCHITEW6432 (SET arch=x86)
 ) 
 IF "%arch%" == "x86" (
-	set ffmpeg="%path%ffmpeg_32.exe"
+	SET ffmpeg="%path%ffmpeg_32.exe"
 )
-%ffmpeg% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null -
+IF "%useSed%" == "TRUE" (
+	ECHO Processing: %1
+	ECHO.
+	%ffmpeg% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null - 2>&1 | %sed% 1,/^\[Parsed_loudnorm/d
+) ELSE (
+	%ffmpeg% -i %1 -af loudnorm=dual_mono=true:print_format=json -nostats -f null -
+)
 ECHO.
 ECHO Copy 'input_lra' value into a tag named 'LRA' within foobar
 PAUSE
