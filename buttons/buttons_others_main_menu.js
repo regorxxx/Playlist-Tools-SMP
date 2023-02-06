8fbff1c74dc467f02b9412d8ca8d035ab21e5ee4
regorxxx <regorxxx@protonmail.com>
2023-02-06 01:51:49 +0100

    - Buttons: added 'main_menu' button which allows to call [multiple] native main menus (dynamically generated ones too). Some kind of automation is allowed by configuring states and unloading actions. For ex. to easily switch playback statistics monitoring on demand for multiple components (database, ListenBrainz, Last.Fm, ...) with a single click, and also ensure they are enabled back when closing Foobar200. After every menu action a custom timeout can be added (for asynchronous actions).

---
+++ buttons/buttons_others_main_menu.js
@@ -0,0 +1,137 @@
+﻿'use strict';
+//05/02/23
+
+/* 
+	Main Menu shortcut
+	----------------
+	Runs multiple main menus with one single click, on order.
+	Also allows to call such menus before closing foobar, according to button state (enabled/disabled).
+	Default button state may be set at startup, and will change when clicking on the button.
+	There is no fancy tracking between sessions though...
+ */
+
+include('..\\helpers\\helpers_xxx.js');
+include('..\\helpers\\buttons_xxx.js');
+include('..\\helpers\\helpers_xxx_file.js');
+include('..\\helpers\\helpers_xxx_input.js');
+include('..\\helpers\\menu_xxx.js');
+include('..\\helpers\\menu_xxx_extras.js');
+var prefix = 'mms';
+
+try {window.DefineScript('Main Menu Shortcut Button', {author:'XXX', version: '1.0.0', features: {drag_n_drop: false}});} catch (e) {/* console.log('Main Menu Shortcut Button loaded.'); */} //May be loaded along other buttons
+
+prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
+
+var newButtonsProperties = { //You can simply add new properties here
+	customName:		['Name for the custom UI button', 'Main Menu', {func: isString}, 'Main Menu'],
+	entries:		['Main menu entries', JSON.stringify([
+		{name: 'Playback Statistics', command: 'Library/Playback Statistics/Monitor playing tracks'},
+		{name: 'ListenBrainz Statistics', command: 'Playback/Submit to ListenBrainz'}
+	]), {func: isJSON}],
+	unloadCall: 	['Call menus on unload options', JSON.stringify({enabled: false, disabled: false}), {func: isJSON}],
+	indicator: 		['Indicator options', JSON.stringify({init: true, enabled: false}), {func: isJSON}]
+};
+newButtonsProperties.entries.push(newButtonsProperties.entries[1]);
+newButtonsProperties.unloadCall.push(newButtonsProperties.unloadCall[1]);
+newButtonsProperties.indicator.push(newButtonsProperties.indicator[1]);
+
+setProperties(newButtonsProperties, prefix, 0); //This sets all the panel properties at once
+newButtonsProperties = getPropertiesPairs(newButtonsProperties, prefix, 0);
+buttonsBar.list.push(newButtonsProperties);
+
+{
+	var newButton = {
+		'Main Menu': new themedButton({x: 0, y: 0, w: _gr.CalcTextWidth(newButtonsProperties.customName[1], _gdiFont('Segoe UI', 12 * buttonsBar.config.scale)) + 35, h: 22}, newButtonsProperties.customName[1], function (mask) {
+			const list = JSON.parse(this.buttonsProperties.entries[1]);
+			const unloadCall = JSON.parse(this.buttonsProperties.unloadCall[1]);
+			const indicator = JSON.parse(this.buttonsProperties.indicator[1]);
+			if (mask === MK_SHIFT) {
+				const menu = new _menu({onBtnUp: () => {
+					this.buttonsProperties.entries[1] = JSON.stringify(list);
+					overwriteProperties(this.buttonsProperties);
+				}});
+				menu.newEntry({entryText: 'Select output:', func: null, flags: MF_GRAYED});
+				menu.newEntry({entryText: 'sep'});
+				_createSubMenuEditEntries(menu, void(0), {
+					name: 'Main Menu',
+					list, 
+					defaults: JSON.parse(this.buttonsProperties.entries[3]), 
+					input : () => {return Input.string('string', '', 'Enter complete menu name:\nEx: Library/Playback Statistics/Monitor playing tracks', window.Name + 'Main Menu Shortcut' , 'Library/Playback Statistics/Monitor playing tracks', void(0), true);}
+				});
+				{
+					const menuName = menu.newMenu('Run when closing foobar?');
+					menu.newEntry({menuName, entryText: 'If state is enabled', func: () => {
+						unloadCall.enabled = !unloadCall.enabled;
+						this.buttonsProperties.unloadCall[1] = JSON.stringify(unloadCall);
+						overwriteProperties(this.buttonsProperties); // Force overwriting
+					}, flags: indicator.enabled ? MF_STRING : MF_GRAYED});
+					menu.newCheckMenu(menuName, 'If state is enabled', void(0), () => {return unloadCall.enabled;});
+					menu.newEntry({menuName, entryText: 'If state is disabled', func: () => {
+						unloadCall.disabled = !unloadCall.disabled;
+						this.buttonsProperties.unloadCall[1] = JSON.stringify(unloadCall);
+						overwriteProperties(this.buttonsProperties); // Force overwriting
+					}});
+					menu.newCheckMenu(menuName, 'If state is disabled', void(0), () => {return unloadCall.disabled;});
+				}
+				{
+					const menuName = menu.newMenu('Button states');
+					menu.newEntry({menuName, entryText: 'Use button states', func: () => {
+						indicator.enabled = !indicator.enabled;
+						this.buttonsProperties.indicator[1] = JSON.stringify(indicator);
+						overwriteProperties(this.buttonsProperties); // Force overwriting
+					}});
+					menu.newCheckMenu(menuName, 'Use button states', void(0), () => {return indicator.enabled;});
+					menu.newEntry({menuName, entryText: 'Set as enabled at startup', func: () => {
+						indicator.init = !indicator.init;
+						this.buttonsProperties.indicator[1] = JSON.stringify(indicator);
+						overwriteProperties(this.buttonsProperties); // Force overwriting
+					}});
+					menu.newCheckMenu(menuName, 'Set as enabled at startup', void(0), () => {return indicator.init;});
+				}
+				menu.newEntry({entryText: 'sep'});
+				menu.newEntry({entryText: 'Rename button...', func: () => {
+					const input = Input.string('string', this.buttonsProperties.customName[1], 'Enter button name:', window.Name + 'Main Menu Shortcut' , this.buttonsProperties.customName[3], void(0), false);
+					if (input === null) {return;}
+					this.buttonsProperties.customName[1] = input;
+					overwriteProperties(this.buttonsProperties); // Force overwriting
+					this.text = input;
+					this.w = _gr.CalcTextWidth(input, _gdiFont('Segoe UI', 12 * buttonsBar.config.scale)) + 30;
+					this.w *= buttonsBar.config.scale;
+					this.changeScale(buttonsBar.config.scale);
+					window.Repaint();
+				}});
+				menu.btn_up(this.currX, this.currY + this.currH);
+			} else {
+				list.forEach((entry) => {
+					console.log(entry.name + ' -> ' + entry.command);
+					try {fb.RunMainMenuCommand(entry.command)} catch (e) {console.log(e);}
+				});
+				if (indicator.enabled) {this.switchActive();}
+			}
+		}, null, void(0), (parent) => {
+			const bShift = utils.IsKeyPressed(VK_SHIFT);
+			const bInfo = typeof menu_panelProperties === 'undefined' || menu_panelProperties.bTooltipInfo[1];
+			let info = 'Main menu';
+			// Entries
+			const list = JSON.parse(parent.buttonsProperties.entries[1]);
+			info += '\nEntries:\t' + list.map(e => e.name).joinEvery(', ', 2, '\n\t');
+			if (bShift || bInfo) {
+				info += '\n-----------------------------------------------------';
+				info += '\n(Shift + L. Click to open config menu)';
+			}
+			return info;
+		}, void(0), newButtonsProperties, chars.music, void(0), void(0),
+		{
+			'on_script_unload': (parent) => {
+				const unloadCall = JSON.parse(parent.buttonsProperties.unloadCall[1]);
+				if (unloadCall.disabled && !parent.active || unloadCall.enabled && parent.active) {
+					parent.onClick();
+				}
+			}
+		}),
+	};
+	// Default state
+	const indicator = JSON.parse(newButton['Main Menu'].buttonsProperties.indicator[1]);
+	if (indicator.init) {newButton['Main Menu'].active = true;}
+	addButton(newButton);
+}
---
+++ helpers/menu_xxx_extras.js
@@ -0,0 +1,66 @@
+﻿'use strict';
+//05/02/23
+
+function _createSubMenuEditEntries(parent, menuName, options /*{name, list, defaults, input, bAdd}*/) {
+	// options.list always point to the original entry list and original values are edited
+	const subMenuSecondName = parent.newMenu('Edit entries from list...' + nextId('invisible', true, false), menuName);
+	options.list.forEach( (entry, index) => {
+		const entryName = (entry.name === 'sep' ? '------(separator)------' : (entry.name.length > 40 ? entry.name.substring(0,40) + ' ...' : entry.name));
+		const subMenuThirdName = parent.newMenu(entryName + nextId('invisible', true, false), subMenuSecondName);
+		parent.newEntry({menuName: subMenuThirdName, entryText: 'Edit entry...', func: () => {
+			const oriEntry = JSON.stringify(entry);
+			let newEntry = oriEntry;
+			try {newEntry = utils.InputBox(window.ID, 'Edit entry as JSON:', scriptName + ': ' + options.name, oriEntry, true);}
+			catch (e) {return;}
+			if (newEntry === oriEntry) {return;}
+			if (!newEntry || !newEntry.length) {fb.ShowPopupMessage('Input: ' + newEntry + '\n\nNon valid entry.', 'JSON error'); return;}
+			try {newEntry = JSON.parse(newEntry);} catch (e) {fb.ShowPopupMessage('Input: ' + newEntry.toString() + '\n\n' + e, 'JSON error'); return;}
+			if (!newEntry) {return;}
+			options.list[index] = newEntry;
+			return options.list;
+		}});
+		parent.newEntry({menuName: subMenuThirdName, entryText: 'Move entry...', func: () => {
+			let pos = 1;
+			try {pos = Number(utils.InputBox(window.ID, 'Move up X indexes (negative is down):\n', scriptName + ': ' + options.name, pos, true));} 
+			catch (e) {return;}
+			if (pos === 0 || !Number.isSafeInteger(pos)) {return;}
+			if (index - pos < 0) {pos = 0;}
+			else if (index - pos >= options.list.length) {pos = options.list.length;}
+			else {pos = index - pos;}
+			options.list.splice(pos, 0, options.list.splice(index, 1)[0]);
+			return options.list;
+		}});
+		parent.newEntry({menuName: subMenuThirdName, entryText: 'sep'});
+		parent.newEntry({menuName: subMenuThirdName, entryText: 'Remove entry', func: () => {
+			options.list.splice(index, 1);
+			return options.list;
+		}});
+	});
+	if (!options.list.length) {parent.newEntry({menuName: subMenuSecondName, entryText: '(none saved yet)', func: null, flags: MF_GRAYED});}
+	parent.newEntry({menuName: subMenuSecondName, entryText: 'sep'});
+	if (!options.hasOwnProperty('bAdd') || options.bAdd) {
+		parent.newEntry({menuName: subMenuSecondName, entryText: 'Add new entry to list...' , func: () => {
+			// Input all variables
+			let input;
+			let entryName = '';
+			try {entryName = utils.InputBox(window.ID, 'Enter name for menu entry\nWrite \'sep\' to add a line.', scriptName + ': ' + options.name, '', true);}
+			catch (e) {return;}
+			if (!entryName.length) {return;}
+			if (entryName === 'sep') {input = {name: entryName};} // Add separator
+			else { // or new entry
+				const entry = options.input();
+				if (!entry) {return;}
+				input = {name: entryName, ...entry}
+			}
+			// Add entry
+			options.list.push(input);
+			return options.list;
+		}});
+	}
+	parent.newEntry({menuName: subMenuSecondName, entryText: 'sep'});
+	parent.newEntry({menuName: subMenuSecondName, entryText: 'Restore defaults...', func: () => {
+		options.list.length = 0;
+		clone(options.defaults).forEach(e => options.list.push(e));
+		return options.list;
+	}});
+}
