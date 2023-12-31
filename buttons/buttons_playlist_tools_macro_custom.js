e68b771d4776c4f46d794b2d1bc046ba7ada87a3
regorxxx <regorxxx@protonmail.com>
2023-12-31 01:29:39 +0100

    - Macros: fixed macros button and custom macro button to work with the latest changes to macros on [3.7.0](#370---2023-12-17). - Macros: custom macro button now has a menu entry to edit the button name (which was previously only editable on first init).

--- buttons/buttons_playlist_tools_macro_custom.js
+++ buttons/buttons_playlist_tools_macro_custom.js
@@ -1,63 +1,91 @@
 ﻿'use strict';
-//15/03/23
+//30/12/23
 
-/* 
+/*
 	Playlist Tools Macro custom
 	-------------------
 	Shortcut to configurable macro from Playlist Tools
  */
 
+/* global menu:readable, menu_panelProperties:readable, menu_prefix:readable, menu_prefix_panel:readable, menu_properties:readable, MF_GRAYED:readable, defaultArgs:readable, MK_SHIFT:readable */
+include('..\\helpers\\helpers_xxx.js');
+/* global globFonts:readable */
 include('..\\helpers\\buttons_xxx.js');
+/* global getUniquePrefix:readable, buttonsBar:readable, addButton:readable, themedButton:readable */
 include('..\\helpers\\helpers_xxx_properties.js');
+/* global setProperties:readable, getPropertiesPairs:readable, overwriteProperties:readable */
+include('..\\helpers\\helpers_xxx_prototypes.js');
+/* global isBoolean:readable, isStringWeak:readable,  */
+include('..\\helpers\\helpers_xxx_UI.js');
+/* global _gdiFont:readable, _gr:readable, _scale:readable, chars:readable */
 include('..\\helpers\\menu_xxx.js');
+/* global _menu:readable */
 
-var prefix = 'mac';
+var prefix = 'mac'; // NOSONAR[global]
 prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
-var newButtonsProperties = { //You can simply add new properties here
-	customName: ['Name for the custom UI button', 'Customize!', {func: isStringWeak}, 'Customize!'],
-	macro: 		['Macro entry', '', {func: isStringWeak}, ''],
-	bIconMode:		['Icon-only mode?', false, {func: isBoolean}, false]
+var newButtonsProperties = { // NOSONAR[global]
+	customName: ['Name for the custom UI button', 'Customize!', { func: isStringWeak }, 'Customize!'],
+	macro: ['Macro entry', '', { func: isStringWeak }, ''],
+	bIconMode: ['Icon-only mode?', false, { func: isBoolean }, false]
 };
 setProperties(newButtonsProperties, prefix, 0); //This sets all the panel properties at once
 newButtonsProperties = getPropertiesPairs(newButtonsProperties, prefix, 0); // And retrieve
 buttonsBar.list.push(newButtonsProperties);
 
 addButton({
-	'Playlist Tools Macros (CUSTOM)': new themedButton({x: 0, y: 0, w: _gr.CalcTextWidth(newButtonsProperties.customName[1], _gdiFont(globFonts.button.name, globFonts.button.size * buttonsBar.config.scale)) + 25 * _scale(1, false) /_scale(buttonsBar.config.scale), h: 22}, newButtonsProperties.customName[1], function (mask) {
+	'Playlist Tools Macros (CUSTOM)': new themedButton({ x: 0, y: 0, w: _gr.CalcTextWidth(newButtonsProperties.customName[1], _gdiFont(globFonts.button.name, globFonts.button.size * buttonsBar.config.scale)) + 25 * _scale(1, false) / _scale(buttonsBar.config.scale), h: 22 }, newButtonsProperties.customName[1], function (mask) {
 		if (isPlaylistToolsLoaded()) {
 			if (mask === MK_SHIFT) {
 				const configMenu = new _menu();
-				const scriptDefaultArgs = {properties: [{...menu_properties}, () => {return menu_prefix;}]};
-				configMenu.newCondEntry({entryText: 'Macros', condFunc: (args = {...scriptDefaultArgs, ...defaultArgs}) => {
-					args.properties = getPropertiesPairs(args.properties[0], args.properties[1](), 0); // Update properties from the panel. Note () call on second arg
-					let propMacros = JSON.parse(args.properties['macros'][1]);
-					if (!macros.length && propMacros.length) {macros = propMacros;} // Restore macros list on first init
-					configMenu.newEntry({entryText: 'Execute macros:', func: null, flags: MF_GRAYED});
-					configMenu.newEntry({entryText: 'sep'});
-					configMenu.newEntry({entryText: 'None', func: () => {
-						this.buttonsProperties['macro'][1] = '';
-						overwriteProperties(this.buttonsProperties);
-					}});
-					configMenu.newEntry({entryText: 'sep'});
-					// List
-					propMacros.forEach((macro) => {
-						if (macro.name === 'sep') { // Create separators
-							configMenu.newEntry({entryText: 'sep'});
-						} else {
-							configMenu.newEntry({entryText: macro.name, func: () => {
-								this.buttonsProperties['macro'][1] = 'Macros\\' + macro.name;
+				const scriptDefaultArgs = { properties: [{ ...menu_properties }, () => { return menu_prefix; }] };
+				const Macros = menu.Macros;
+				configMenu.newCondEntry({
+					entryText: 'Macros', condFunc: (args = { ...scriptDefaultArgs, ...defaultArgs }) => {
+						args.properties = getPropertiesPairs(args.properties[0], args.properties[1](), 0); // Update properties from the panel. Note () call on second arg
+						let propMacros = JSON.parse(args.properties['macros'][1]);
+						Macros.set(propMacros); // Restore macros list on first init
+						configMenu.newEntry({ entryText: 'Execute macros:', func: null, flags: MF_GRAYED });
+						configMenu.newEntry({ entryText: 'sep' });
+						configMenu.newEntry({
+							entryText: 'None', func: () => {
+								this.buttonsProperties['macro'][1] = '';
+								overwriteProperties(this.buttonsProperties);
+							}
+						});
+						configMenu.newEntry({ entryText: 'sep' });
+						// List
+						propMacros.forEach((macro) => {
+							if (macro.name === 'sep') { // Create separators
+								configMenu.newEntry({ entryText: 'sep' });
+							} else {
+								configMenu.newEntry({
+									entryText: macro.name, func: () => {
+										this.buttonsProperties['macro'][1] = 'Macros\\' + macro.name;
+										overwriteProperties(this.buttonsProperties); // Force overwriting
+									}
+								});
+							}
+						});
+						const options = propMacros.map((item) => { return item.name; }).filter((item) => { return item !== 'sep'; });
+						configMenu.newCheckMenu(configMenu.getMainMenuName(), 'None', options[options.length - 1], () => {
+							const name = this.buttonsProperties['macro'][1].replace('Macros\\', '');
+							const idx = options.indexOf(name);
+							return idx !== -1 ? idx + 1 : 0;
+						});
+						if (!propMacros.length) { configMenu.newEntry({ entryText: '(none saved yet)', func: null, flags: MF_GRAYED }); }
+						configMenu.newEntry({ entryText: 'sep' });
+						configMenu.newEntry({ entryText: 'Button name...', func: () => {
+							const newName = utils.InputBox(window.ID, 'Enter button name:', window.Name + ': Customizable Playlist Tools Macro Button', this.buttonsProperties.customName[1]).toString();
+							if (!newName.length) {
+								return;
+							} else {
+								this.buttonsProperties.customName[1] = newName;
 								overwriteProperties(this.buttonsProperties); // Force overwriting
-							}});
-						}
-					});
-					const options = propMacros.map((item) => {return item.name;}).filter((item) => {return item !== 'sep';})
-					configMenu.newCheckMenu(configMenu.getMainMenuName(), 'None', options[options.length - 1], () => {
-						const name = this.buttonsProperties['macro'][1].replace('Macros\\','');
-						const idx = options.indexOf(name);
-						return idx !== -1 ? idx + 1 : 0;
-					});
-					if (!propMacros.length) {configMenu.newEntry({entryText: '(none saved yet)', func: null, flags: MF_GRAYED});}
-				}});
+								this.adjustNameWidth(newName);
+							}
+						}});
+					}
+				});
 				configMenu.btn_up(this.currX, this.currY + this.currH);
 			} else {
 				if (this.buttonsProperties['customName'][1] === 'Customize!') {
@@ -70,20 +98,21 @@ addButton({
 						this.adjustNameWidth(newName);
 					}
 				} else if (this.buttonsProperties['macro'][1].length) {
-					menu.btn_up(void(0), void(0), void(0), this.buttonsProperties['macro'][1]); // Don't clear menu on last call
+					menu.btn_up(void (0), void (0), void (0), this.buttonsProperties['macro'][1]); // Don't clear menu on last call
 				}
 			}
-		} else {fb.ShowPopupMessage('WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS', 'Playlist Tools');}
-	}, null, void(0), (parent) => {
-		return (isPlaylistToolsLoaded() 
-			? 'Executes Playlist Tools Menu assigned macros:\nEntry:\t' + (parent.buttonsProperties.macro[1] || '-None-') + (
-				getPropertiesPairs(menu_panelProperties, menu_prefix_panel, 0).bTooltipInfo[1] 
-					? '\n-----------------------------------------------------\n(L. Click to execute macro)\n(Shift + L. Click to configure macro)' 
+		} else { fb.ShowPopupMessage('WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS', 'Playlist Tools'); }
+	}, null, void (0), (parent) => {
+		return (isPlaylistToolsLoaded()
+			? 'Executes Playlist Tools Menu assigned macros:\nEntry:\t' + (parent.buttonsProperties.macro[1] || '-None-') +
+			(
+				getPropertiesPairs(menu_panelProperties, menu_prefix_panel, 0).bTooltipInfo[1]
+					? '\n-----------------------------------------------------\n(L. Click to execute macro)\n(Shift + L. Click to configure macro)'
 					: ''
-				) 
+			)
 			: 'WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS');
 	}, null, newButtonsProperties, chars.hourglassHalf),
 });
 
 // Helpers
-function isPlaylistToolsLoaded() {return (typeof specialMenu !== 'undefined' && typeof configMenu !== 'undefined' && typeof scriptName !== 'undefined' && typeof menu !== 'undefined');}
+function isPlaylistToolsLoaded() { return (typeof specialMenu !== 'undefined' && typeof configMenu !== 'undefined' && typeof scriptName !== 'undefined' && typeof menu !== 'undefined'); }
--- buttons/buttons_playlist_tools_macros.js
+++ buttons/buttons_playlist_tools_macros.js
@@ -1,59 +1,86 @@
 ﻿'use strict';
-//15/03/23
+//30/12/23
 
-/* 
+/*
 	Playlist Tools Macros
 	-------------------
 	MaCros submenu from Playlist Tools
  */
 
+/* global menu:readable, menu_panelProperties:readable, menu_prefix:readable, menu_prefix_panel:readable, menu_properties:readable, MF_GRAYED:readable, defaultArgs:readable */
+include('..\\helpers\\helpers_xxx.js');
+/* global globFonts:readable */
 include('..\\helpers\\buttons_xxx.js');
+/* global getUniquePrefix:readable, buttonsBar:readable, addButton:readable, themedButton:readable */
+include('..\\helpers\\helpers_xxx_properties.js');
+/* global setProperties:readable, getPropertiesPairs:readable */
+include('..\\helpers\\helpers_xxx_prototypes.js');
+/* global isBoolean:readable */
+include('..\\helpers\\helpers_xxx_UI.js');
+/* global _gdiFont:readable, _gr:readable, _scale:readable, chars:readable */
 include('..\\helpers\\menu_xxx.js');
-var prefix = 'ptm';
+/* global _menu:readable */
+
+var prefix = 'ptm'; // NOSONAR[global]
 
 prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
-var newButtonsProperties = { //You can simply add new properties here
-	bIconMode:	['Icon-only mode?', false, {func: isBoolean}, false]
+var newButtonsProperties = { // NOSONAR[global]
+	bIconMode: ['Icon-only mode?', false, { func: isBoolean }, false]
 };
 setProperties(newButtonsProperties, prefix, 0); //This sets all the panel properties at once
 newButtonsProperties = getPropertiesPairs(newButtonsProperties, prefix, 0);
 buttonsBar.list.push(newButtonsProperties);
 
 addButton({
-	'Playlist Tools Macros': new themedButton({x: 0, y: 0, w: _gr.CalcTextWidth('Macros', _gdiFont(globFonts.button.name, globFonts.button.size * buttonsBar.config.scale)) + 30 * _scale(1, false) /_scale(buttonsBar.config.scale), h: 22}, 'Macros', function () {
+	'Playlist Tools Macros': new themedButton({ x: 0, y: 0, w: _gr.CalcTextWidth('Macros', _gdiFont(globFonts.button.name, globFonts.button.size * buttonsBar.config.scale)) + 30 * _scale(1, false) / _scale(buttonsBar.config.scale), h: 22 }, 'Macros', function () {
 		if (isPlaylistToolsLoaded()) {
 			const configMenu = new _menu();
-			const scriptDefaultArgs = {properties: [{...menu_properties}, () => {return menu_prefix;}]};
-			configMenu.newCondEntry({entryText: 'Macros', condFunc: (args = {...scriptDefaultArgs, ...defaultArgs}) => {
-				args.properties = getPropertiesPairs(args.properties[0], args.properties[1](), 0); // Update properties from the panel. Note () call on second arg
-				let propMacros = JSON.parse(args.properties['macros'][1]);
-				if (!macros.length && propMacros.length) {macros = propMacros;} // Restore macros list on first init
-				configMenu.newEntry({entryText: 'Execute macros:', func: null, flags: MF_GRAYED});
-				configMenu.newEntry({entryText: 'sep'});
-				// List
-				propMacros.forEach((macro) => {
-					if (macro.name === 'sep') { // Create separators
-						configMenu.newEntry({entryText: 'sep'});
-					} else {
-						configMenu.newEntry({entryText: macro.name, func: () => {
-							menu.btn_up(void(0), void(0), void(0), 'Macros\\' + macro.name); // Don't clear menu on last call
-						}});
-					}
-				});
-				if (!propMacros.length) {configMenu.newEntry({entryText: '(none saved yet)', func: null, flags: MF_GRAYED});}
-			}});
+			const scriptDefaultArgs = { properties: [{ ...menu_properties }, () => { return menu_prefix; }] };
+			const Macros = menu.Macros;
+			configMenu.newCondEntry({
+				entryText: 'Macros', condFunc: (args = { ...scriptDefaultArgs, ...defaultArgs }) => {
+					args.properties = getPropertiesPairs(args.properties[0], args.properties[1](), 0); // Update properties from the panel. Note () call on second arg
+					let propMacros = JSON.parse(args.properties['macros'][1]);
+					Macros.set(propMacros);
+					configMenu.newEntry({ entryText: 'Execute macros:', func: null, flags: MF_GRAYED });
+					configMenu.newEntry({ entryText: 'sep' });
+					// List
+					const entryNames = new Set();
+					propMacros.forEach((macro) => {
+						if (macro.name === 'sep') { // Create separators
+							configMenu.newEntry({ entryText: 'sep' });
+						} else {
+							let macroName = macro.name || '';
+							macroName = macroName.length > 40
+								? macroName.substring(0,40) + ' ...'
+								: macroName;
+							if (entryNames.has(macroName)) {
+								fb.ShowPopupMessage('There is an entry with duplicated name:\t' + macroName + '\nEdit the custom entries and either remove or rename it.\n\nEntry:\n' + JSON.stringify(macro, null, '\t'), 'Playlist Tools: Macros');
+								return;
+							} else {entryNames.add(macroName);}
+							configMenu.newEntry({
+								entryText: macro.name, func: () => {
+									menu.btn_up(void (0), void (0), void (0), 'Macros\\' + macroName); // Don't clear menu on last call
+								}
+							});
+						}
+					});
+					if (!propMacros.length) { configMenu.newEntry({ entryText: '(none saved yet)', func: null, flags: MF_GRAYED }); }
+				}
+			});
 			configMenu.btn_up(this.currX, this.currY + this.currH);
-		} else {fb.ShowPopupMessage('WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS', 'Playlist Tools');}
-	}, null, void(0), () => {
-		return (isPlaylistToolsLoaded() 
-			? 'Executes Playlist Tools Menu macros' + (
-				getPropertiesPairs(menu_panelProperties, menu_prefix_panel, 0).bTooltipInfo[1] 
-					? '\n-----------------------------------------------------\n(L. Click to show list)' 
+		} else { fb.ShowPopupMessage('WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS', 'Playlist Tools'); }
+	}, null, void (0), () => {
+		return (isPlaylistToolsLoaded()
+			? 'Executes Playlist Tools Menu macros' +
+			(
+				getPropertiesPairs(menu_panelProperties, menu_prefix_panel, 0).bTooltipInfo[1]
+					? '\n-----------------------------------------------------\n(L. Click to show list)'
 					: ''
-				) 
+			)
 			: 'WARNING! CAN\'T USE THIS BUTTON WITHOUT PLAYLIST TOOLS');
 	}, prefix, newButtonsProperties, chars.hourglassHalf),
 });
 
 // Helpers
-function isPlaylistToolsLoaded() {return (typeof specialMenu !== 'undefined' && typeof configMenu !== 'undefined' && typeof scriptName !== 'undefined' && typeof menu !== 'undefined');}
+function isPlaylistToolsLoaded() { return (typeof specialMenu !== 'undefined' && typeof configMenu !== 'undefined' && typeof scriptName !== 'undefined' && typeof menu !== 'undefined'); }
