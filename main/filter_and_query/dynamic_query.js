d31df5aab3b1886ffe2c5b1546afcf77a6e44000
regorxxx <regorxxx@protonmail.com>
2024-01-24 23:12:30 +0100

    - Helpers: updated helpers.

--- main/filter_and_query/dynamic_query.js
+++ main/filter_and_query/dynamic_query.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//16/01/24
+//21/01/24
 
 /* exported dynamicQuery */
 
@@ -15,6 +15,15 @@ include('..\\..\\helpers\\helpers_xxx_tags.js');
 include('..\\sort\\harmonic_mixing.js');
 /* global queryReplaceKeys:readable */
 
+/**
+ * Processes the dynamic query against the handleList or handle and outputs a handleList or sends results to a playlist.
+ *
+ * @function
+ * @name dynamicQuery
+ * @kind function
+ * @param {{ query?: string sort?: { tfo?: string direction: number } handle?: FbMetadbHandle handleList?: FbMetadbHandleList playlistName?: string bSendToPls?: boolean source?: FbMetadbHandleList  bToLowerCase?: boolean  }} { query, sort, handle, handleList, playlistName, bSendToPls, source, bToLowerCase }? [{ query = 'ARTIST IS #ARTIST#', sort = { tfo: null, direction: 1 }, handle = fb.GetFocusItem(true), handleList = null, playlistName = 'Search...', bSendToPls = true, source = null, bToLowerCase = false } = {}]
+ * @returns {FbMetadbHandleList | null}
+ */
 function dynamicQuery({ query = 'ARTIST IS #ARTIST#', sort = { tfo: null, direction: 1 }, handle = fb.GetFocusItem(true), handleList = null, playlistName = 'Search...', bSendToPls = true, source = null, bToLowerCase = false } = {}) {
 	query = dynamicQueryProcess({ query, handle, handleList, bToLowerCase });
 	if (!query) { return null; }
@@ -27,6 +36,15 @@ function dynamicQuery({ query = 'ARTIST IS #ARTIST#', sort = { tfo: null, direct
 	return outputHandleList;
 }
 
+/**
+ * Processes the query string and replaces any placeholders (within #) with actual values from the handleList or handle.
+ *
+ * @function
+ * @name dynamicQueryProcess
+ * @kind function
+ * @param {{ query?: string handle?: FbMetadbHandle handleList?: FbMetadbHandleList bToLowerCase?: boolean }} { query, handle, handleList, bToLowerCase }? [{ query = 'ARTIST IS #ARTIST#', handle = fb.GetFocusItem(true), handleList = null, bToLowerCase = false }={}]
+ * @returns {string | null}
+ */
 function dynamicQueryProcess({ query = 'ARTIST IS #ARTIST#', handle = fb.GetFocusItem(true), handleList = null, bToLowerCase = false } = {}) {
 	if (!query || !query.length) { return null; }
 	if (query.indexOf('#') !== -1) {
@@ -34,13 +52,13 @@ function dynamicQueryProcess({ query = 'ARTIST IS #ARTIST#', handle = fb.GetFocu
 		else if (handleList) {
 			const queries = [...new Set(handleList.Convert().map((handle) => {
 				return /#NEXTKEY#|#PREVKEY#/.test(query)
-					? queryReplaceWithCurrent(queryReplaceKeys(query, handle), handle, null, {bToLowerCase})
-					: queryReplaceWithCurrent(query, handle, null, {bToLowerCase});
+					? queryReplaceWithCurrent(queryReplaceKeys(query, handle), handle, null, { bToLowerCase })
+					: queryReplaceWithCurrent(query, handle, null, { bToLowerCase });
 			}))];
 			query = queryJoin(queries, 'OR');
 		} else if (handle) {
 			if (/#NEXTKEY#|#PREVKEY#/.test(query)) { query = queryReplaceKeys(query, handle); }
-			query = queryReplaceWithCurrent(query, handle, null, {bToLowerCase});
+			query = queryReplaceWithCurrent(query, handle, null, { bToLowerCase });
 		}
 	}
 	try { fb.GetQueryItems(new FbMetadbHandleList(), query); }
